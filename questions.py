9b40ad2511f53f3e771a9572582fad529831613a
varenyamsingh <varenyamsingh1330@gmail.com>
2024-12-24 22:10:58 +0530

    commit

--- questions.py
+++ questions.py
@@ -1,4 +1,4 @@
-# write a program to display a person's name, age and address in fifferent lines
+# write a program to display a person's name, age and address in different lines
 '''name = "Tony"
 age = 23
 address = "Delhi"
@@ -21,16 +21,38 @@ print(a, b)'''
 
 
 # Convert float to int
-num = 23.3
+'''num = 23.3
 num = int(num)
-print(type(num))
+print(type(num))'''
 
 
 # take input as int and then convert it to float
-num = int(input("Enter a number: "))
+'''num = int(input("Enter a number: "))
 num = float(num)
 print(num)
-print(type(num))
+print(type(num))'''
+
+
+# Sum of all even numbers upto 50
+'''sum = 0
+for i in range(1, 51):
+    if i % 2 == 0:
+        sum += i
+print("sum of all even numbers upto 50 is: ", sum)'''
+
+
+# Write first 20 numbers and their squared numbers
+'''for i in range(1, 21):
+    print(i, i*i)'''
+    
+    
+# Find sum of first 10 odd numbers
+sum = 0
+for i in range(1, 11):
+    if i % 2 != 0:
+        sum += i
+print(sum)
+
 
 
 
