10e4a22bfdfb458355b5d6298323d77042f1314c
varenyamsingh <varenyamsingh1330@gmail.com>
2024-12-25 19:00:12 +0530

    commit

--- questions.py
+++ questions.py
@@ -47,12 +47,39 @@ print("sum of all even numbers upto 50 is: ", sum)'''
     
     
 # Find sum of first 10 odd numbers
-sum = 0
+'''sum = 0
 for i in range(1, 11):
     if i % 2 != 0:
         sum += i
-print(sum)
+print(sum)'''
 
 
+# Length of string
+a = "Hello World in, python {}"
+print(len(a)) 
+print(a.index("o", 5, 11)) # last two numbers are index numbers
+print(a.capitalize())
+print(a.format(a))
+# Check the occurence of any alphabet 
+print(a.count("o"))
+print(a.lower())
+print(a.upper())
+print(a.title()) # makes first word in capital
+print(a.find("in")) # find any word in sentence
+print(a.center(5, "*"))
+
+
+# Pattern Problem
+'''for i in range(1, 6):
+    for j in range(1, i+1):
+        print(i, end=" ")
+    print()'''
+
+
+# Pattern Problem
+'''for i in range(1, 6):
+    for j in range(6, i, -1):
+        print(j, end=" ")
+    print()'''
 
 
